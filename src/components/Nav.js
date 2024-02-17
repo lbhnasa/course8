91ec682149509244aa65fd6c1c741aa7d31f8e09
lbhnasa <lbhnasa@gmail.com>
2024-02-16 22:02:50 -0800

    added multiple components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,10 @@ import React from "react";
 function Nav() {
     return
     <>
+    <Routes> 
+    <Route path="/" element={<HomePage />}></Route>
+    <Route path="/booking" element={<BookingPage />}></Route>
+    </Routes>
     </>
 }
 
